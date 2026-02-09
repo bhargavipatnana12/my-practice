d1619499e6f65bd91459bfa37efdd59f20d48fd1
bhargavipatnana12 <bhargavipatnana12@gmail.com>
2026-02-09 16:15:38 +0530

    CLASS COMPONENTS

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Rating from "@mui/material/Rating";
-import Typography from "@mui/material/Typography";
+import React, { Component } from "react";
 
-export default function BasicRating() {
-  const [value, setValue] = React.useState(2);
-
-  return (
-    <Box sx={{ "& > legend": { mt: 2 } }}>
-      <Typography component="legend">Controlled</Typography>
-      <Rating
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
-      <Typography component="legend">Uncontrolled</Typography>
-      <Rating
-        name="simple-uncontrolled"
-        onChange={(event, newValue) => {
-          console.log(newValue);
-        }}
-        defaultValue={2}
-      />
-      <Typography component="legend">Read only</Typography>
-      <Rating name="read-only" value={value} readOnly />
-      <Typography component="legend">Disabled</Typography>
-      <Rating name="disabled" value={value} disabled />
-      <Typography component="legend">No rating given</Typography>
-      <Rating name="no-value" value={null} />
-    </Box>
-  );
+export default class App extends Component {
+  state = { name: "skillhub" };
+  render() {
+    return (
+      <div>
+        <center>
+          <h1>{this.state.name}</h1>
+          <button onClick={() => this.setState({ name: "telugu skillhub" })}>
+            Change
+          </button>
+        </center>
+      </div>
+    );
+  }
 }
