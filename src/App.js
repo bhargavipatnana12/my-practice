c2788c53ca0283a048d6049b1dcbc18fc895a5dc
bhargavipatnana12 <bhargavipatnana12@gmail.com>
2026-02-11 15:56:02 +0530

    just hello

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import { useReducer } from "react";
 import React from "react";
 
-const initialState = { count: 0 };
-function reducer(state, action) {
-  switch (action.type) {
-    case "increment":
-      return { Count: state.count + 1 };
-    case "decrement":
-      return { Count: state.count - 1 };
-    default:
-      throw new Error();
-  }
-}
-function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+const App = () => {
   return (
     <div>
-      <center>
-        Count :{state.count}
-        <button onClick={() => dispatch({ type: "increment" })}>+</button>
-        <button onClick={() => dispatch({ type: "decrement" })}>-</button>
-      </center>
+      <h1>hello</h1>
     </div>
   );
-}
+};
+
 export default App;
